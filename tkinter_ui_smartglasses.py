abfc016f4799d1e3ef242305ac78090933366606
Teemu Laurila <laurilateemu8@gmail.com>
2021-07-18 09:36:04 +0300

    Create tkinter_ui_smartglasses.py

---
+++ tkinter_ui_smartglasses.py
@@ -0,0 +1,88 @@
+import sys
+import os
+
+from tkinter import *
+from tkinter import font as tkFont
+from pynput import keyboard
+
+# Create the tkinter window
+root = Tk()
+root.state('zoomed')
+
+def chrome():
+    os.system("start chrome https://www.youtube.com/watch?v=_U8NGd2awtI&t=251s")
+    hide()
+
+def hide():
+    global hidden
+
+    root.withdraw()
+    hidden = True
+    print("> hide window")
+
+def show():
+    global hidden
+
+    root.deiconify()
+    root.state('zoomed')
+    hidden = False
+    print("> show window")
+
+def on_press(key):
+    try:
+        print('alphanumeric key {0} pressed'.format(
+            key.char))
+    except AttributeError:
+        print('special key {0} pressed'.format(
+            key))
+
+        if key == key.ctrl_l:
+            show()
+
+for i in range(0, 2):
+    root.columnconfigure(i, weight=1)
+    root.rowconfigure(i, weight=1, )
+
+helv = tkFont.Font(family='Helvetica', size=80, weight='bold')
+
+# Create button to hide
+button = Button(background="#BFC9CA", text="HIDE", font=helv, command=hide)
+button.grid(row=0,column=0,sticky=N+S+E+W)
+
+# Create button to open chrome
+button2 = Button(background="#E74C3C", text="CHROME", font=helv, command=chrome)
+button2.grid(row=0,column=1,sticky=N+S+E+W)
+
+# Create button to 
+button = Button(background="#A9DFBF", text="?", font=helv, command=print())
+button.grid(row=1,column=0,sticky=N+S+E+W)
+
+# Create button to 
+button2 = Button(background="#1ABC9C", text="?", font=helv, command=print())
+button2.grid(row=1,column=1,sticky=N+S+E+W)
+
+hidden = False
+
+try:
+    while True:
+        root.update_idletasks()
+        root.update()
+
+        # If the window is hidden, wait for CTRL to be pressed
+        # and then show the window again
+        if hidden:
+            with keyboard.Events() as events:
+                # Block at most one second
+                event = events.get(1.0)
+                if event is None:
+                    pass
+                    #print('You did not press a key within one second')
+                else:
+                    print('Received event {}'.format(event))
+                    print(event.key)
+                    if event.key == event.key.ctrl_l:
+                        show()
+
+except Exception as e:
+    print(f"CYA: {e}")
+    
